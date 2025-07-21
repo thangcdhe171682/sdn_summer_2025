1f6c325036927296a1943f504ac05d1bf6d29072
thangcdhe171682 <thangcdhe171682@fpt.edu.vn>
2025-07-21 21:07:28 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  isVerified: { type: Boolean, default: false },
-  verifyToken: String
-}, { timestamps: true });
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  avatarUrl: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+});
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema, "User");
+module.exports = User;
